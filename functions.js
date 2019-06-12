861fccc1acd6c445a0da3254e6ec72254624e552
HarryCandlish <hcnd90@gmail.com>
2019-06-13 09:28:31 +1200

    accessed a function within an object and calls that function

--- functions.js
+++ functions.js
@@ -4,6 +4,19 @@ function callsFunction(n) {
 
 // takes function as a parameter and calls that parameter
 
+function callsProperty(obj) {
+  return obj.increment();
+}
+
+// {
+//   n: 0,
+//   increment: function () {
+//     this.n++
+//   }
+
+// obj = accessing the increment function within the callsProperty function which adds 1 to the (n) property.
+
 module.exports = {
-  callsFunction: callsFunction
+  callsFunction: callsFunction,
+  callsProperty: callsProperty
 };
